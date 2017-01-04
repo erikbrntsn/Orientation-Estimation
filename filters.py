2fb47ad35af818cae7f71f934f6a55dc58af9bd9
erikbrntsn <erikbrntsn@gmail.com>
2017-01-04 16:06:05 +0100

    Common and Mahony takes quaternion from vector observation as argument itstead of calculating it them selves. Acc and Mag measurements are removed as arguments to the update functions

--- filters.py
+++ filters.py
@@ -9,44 +9,29 @@ def load_src(name, fpath):
     return imp.load_source(name, os.path.join(os.path.dirname(__file__), fpath))
 
 
-mt = load_src('mathTools', 'quaternion/quaternionUtils.py')
+mt = load_src('mathTools', 'quaternion/quaternion.py')
 
 
 class CommonAxisEstimator(object):
     def __init__(self,
                  beta=0.005,
                  zeta=0.000005,
-                 q=mt.Quaternion(),
-                 mE=np.array([1.0, 2.0, 3.0] / np.sqrt(14)),
-                 gE=np.array([0.0, 0.0, 1.0])):
+                 q=mt.Quaternion()):
         # Current estimate of orientation given as a quaternion
         self.q = q
+        # Gyroscope bias estiamte
+        self.gyrBias = mt.Quaternion()
         # Weighting between quaternion estimated from vector observations and the quaternion
         # derivative obtained from the gyroscope
         self.beta = beta
-        # Gravity in the Earth frame
-        self.gE = gE
-        # Earth's magnetic field in the Earth frame
-        self.mE = mE
-        # Gyroscope bias estiamte
-        self.gyrBias = mt.Quaternion()
         # Low pass filter weight: [0, 1], [no change, full change]
         self.zeta = zeta
 
-    def update(self, acc, gyr, mag, dt):
-        acc /= np.linalg.norm(acc)
-        mag /= np.linalg.norm(mag)
+    def update(self, gyr, qVec dt):
         gyrQ = mt.Quaternion(0, *gyr)
 
-        # Calculate quaternion that minimizes Wahba's problem
-        qWahba = davenportsQMethod(np.hstack((acc[:, None], mag[:, None])),
-                                   np.hstack((self.gE[:, None], self.mE[:, None])))
-
-        self.qWahba = correctQuaFromVecObs(qWahba, self.q)
-        qWahba = self.qWahba
-
         # Bias estimation
-        dqv = (qWahba - self.q) / dt
+        dqv = (qVec - self.q) / dt
         dq_ = 0.5 * self.q * gyrQ
         bias = 2 * self.q.conjugate() * (dq_ - dqv)
         self.gyrBias += self.zeta * (bias - self.gyrBias)
@@ -60,53 +45,37 @@ class CommonAxisEstimator(object):
         qIntegrated = self.q + dq * dt
         qIntegrated.normalize()
         # If the two quaternions are the same, then just use one instead of weighting them
-        if qWahba.dot(qIntegrated) > 0.9999:
-            self.q = qWahba
+        if qVec.dot(qIntegrated) > 0.9999:
+            self.q = qVec
         else:
-            self.q = slerp(qIntegrated, qWahba, self.beta)
+            self.q = slerp(qIntegrated, qVec, self.beta)
             self.q.normalize()
 
 
 class MahonyEstimator(object):
 
     def __init__(self,
-                 kEst=100,
-                 kB=1000,
-                 q=mt.Quaternion(),
-                 mE=np.array([1.0, 2.0, 3.0] / np.sqrt(14)),
-                 gE=np.array([0.0, 0.0, 1.0])):
+                 beta=100,
+                 zeta=1000,
+                 q=mt.Quaternion()):
         # Current estimate of orientation given as a quaternion
         self.q = q
-        # Gravity in the Earth frame
-        self.gE = gE
-        # Earth's magnetic field in the Earth frame
-        self.mE = mE
-        # Weight applied to vector orientation estimate
-        self.kEst = kEst
-        # Weight used in gyro bias estimate
-        self.kB = kB
         # Gyroscope bias estimate
         self.gyrBias = np.array([0.0, 0.0, 0.0])
+        # Weight applied to vector orientation estimate
+        self.beta = beta
+        # Weight used in gyro bias estimate
+        self.zeta = zeta
 
-    def update(self, acc, gyr, mag, dt):
-        # Prepare measurements
-        acc /= np.linalg.norm(acc)
-        mag /= np.linalg.norm(mag)
-
-        # Calculate orientation using the two field observations
-        self.qWahba = davenportsQMethod(np.hstack((acc[:, None], mag[:, None])),
-                                        np.hstack((self.gE[:, None], self.mE[:, None])))
-
+    def update(self, gyr, qVec, dt):
         # Estimate quaternion error
-        qTilde = self.q.conjugate() * self.qWahba
+        qTilde = self.q.conjugate() * qVec
         # Field quaternion correction
         sv = qTilde[0] * qTilde[1:]
-
         # Gyrobias estimation
-        self.gyrBias -= self.kB * sv * dt
-
+        self.gyrBias -= self.zeta * sv * dt
         # Corrected rate of change of quaternion
-        dq = 0.5 * self.q * mt.Quaternion(0, *gyr - self.gyrBias + self.kEst * sv)
+        dq = 0.5 * self.q * mt.Quaternion(0, *gyr - self.gyrBias + self.beta * sv)
         self.q += dq * dt
         self.q.normalize()
 
